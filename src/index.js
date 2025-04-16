4fbd076485c33828fa4af26b5ecabd3d434dcbb6
Tony <shpenov1@gmail.com>
2025-04-16 23:44:33 +0500

    Initial commin

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './style.css';
+
+fetch('http://localhost:3000/contacts')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((item) => {
+            const divContactsContainer = document.body.querySelector('.contacts-container');
+            const divContact = document.createElement("div");
+            const h2Name = document.createElement("h2");
+            const divPhoneMail = document.createElement("div");
+            const pMail = document.createElement("p");
+            const pPhone = document.createElement("p");
+
+            divContact.setAttribute("class", "contacts-container__contact");
+            divPhoneMail.setAttribute("class", "contacts-container__phone-email");
+
+
+            h2Name.textContent = item["name"];
+            pMail.textContent = item["email"];
+            pPhone.textContent = item["phone"];
+
+            divPhoneMail.append(pPhone, pMail);
+            divContact.append(h2Name, divPhoneMail);
+            divContactsContainer.append(divContact);
+        })
+    })
